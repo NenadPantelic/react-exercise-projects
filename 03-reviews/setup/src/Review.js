1ced4d464301123636d2569dfef2bee993ca39c4
NenadPantelic <nenadpantelickg@gmail.com>
2021-08-11 14:31:40 +0200

    Complete reviews exercise

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,61 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  console.log(people);
+  const { name, job, image, text } = people[index];
+
+  const prevPerson = () => {
+    let prevIndex = index - 1;
+    if (prevIndex < 0) {
+      prevIndex = people.length - 1;
+    }
+    setIndex(prevIndex);
+  };
+
+  const nextPerson = () => {
+    setIndex((index + 1) % people.length);
+  };
+
+  const randomPerson = () => {
+    const size = people.length;
+    let randomIndex = getRandomInt(size);
+    while (randomIndex === index) {
+      randomIndex = getRandomInt(size);
+    }
+    setIndex(randomIndex);
+  };
+
+  const getRandomInt = (limit) => {
+    return Math.floor(Math.random() * limit);
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={() => prevPerson()}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={() => nextPerson()}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={() => randomPerson()}>
+        Surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
